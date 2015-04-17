0e61df7145da20d4ef5cd2345312a080820628f8
Brandon Cannaday <brandon@modulus.io>
2015-04-16 20:30:22 -0400

    Added wget.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+apt-get update
+apt-get install -y wget
+
 # Create the mop user. All user code execution runs in this user.
 groupadd -r mop -g 444
 useradd -u 441 -r -g mop -d /mnt/home -s /bin/bash -c "Modulus Image User" mop
